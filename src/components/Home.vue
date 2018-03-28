f379e850884d14d3656e3c0a9676b55a9aa38ceb
Bruno Henry <brunohenryt@hotmail.com>
2018-03-28 19:50:09 -0300

    fist commit

---
+++ src/components/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <Page class="page">
+    <ActionBar title="RadSideDrawer"></ActionBar>
+    <RadSideDrawer ref="drawer"  drawerTransition="PushTransition">
+
+      <StackLayout ~mainContent>
+        <Label :text="mainContentText" textWrap="true" fontSize="13" padding="10" />
+      </StackLayout>
+    </RadSideDrawer>
+  </Page>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                mainContentText: "SideDrawer for NativeScript can be easily setup in the XML definition of your page by defining main- and drawer-content. The component"
+                + " has a default transition and position and also exposes notifications related to changes in its state. Swipe from left to open side drawer.",
+
+            };
+        },
+    };
+</script>
+
+<style scoped>
+  Label {
+    color: red;
+  }
+</style>
